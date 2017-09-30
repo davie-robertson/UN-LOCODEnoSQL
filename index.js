66c9f61e1f851a90b49206b00d3919f83892e8f7
Davie Robertson <me@davie.com>
2017-09-30 18:17:18 +0400

    working emiter (ES6) and example file

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const code = require('./locodenosql');
-const events = require('events');
-const eventEmitter = new events.EventEmitter();
-let results='';
-results=code.getLocations('./UNLOCODE.csv');
-results.on('recordFound', (location) => {
-    console.log(location);
-}
-);
+const UNLOCODE = require('./locodenosql');
+const GetLocations = new UNLOCODE();
+
+GetLocations.on('recordFound', (data) => {
+  /**
+ * A complete country and associated locations have been returned in 
+ * a JSON string (data). These can now be added to a NONSQL document 
+ */
+
+  console.log(`Received data: "${data}"`);
+});
+GetLocations.on('done', (data) => {
+  /*
+All locationes have now been imported
+*/
+  console.log(`done: "${data}"`);
+});
+
+GetLocations.importLocations('./UNLOCODE.csv');
--- locodenosql.js
+++ locodenosql.js
@@ -1,73 +1,78 @@
 const fs = require('fs');
 const _ = require('lodash');
 const readLine = require('readline');
-const events = require('events');
-const eventEmitter = new events.EventEmitter();
-module.exports = {
-    getLocations: getLocations,
-  };
-
+const EventEmitter = require('events');
 
 /**
- * Generate a JSON string for each UNLOCODE
+ * GetLocations in a library to parse the UNLOCODE ("United Nations Code
+ * for Trade and Transport Locations") file(s) available for download as
+ * a series of CSV files from https://www.unece.org/cefact/locode/welcome.html
+ */
+class GetLocations extends EventEmitter {
+  /**
+   * Not sure why I am forced to put this comment here by ESLINT
+   */
+  constructor() {
+    super();
+  }
+  /**
+ * Method to parse CSV file (fileName) and generate a JSON string for each
+ * country and all assocaiated UNLOCODEs within the country
  * @param {string} fileName full name and path to .csv file to be parsed
  */
-function getLocations(fileName) {
-  let columnNames = [
-    'change',
-    'country',
-    'location',
-    'name',
-    'nameSansDiacritics',
-    'subdivision',
-    'function',
-    'status',
-    'date',
-    'iata',
-    'coordinates',
-    'remarks'
-  ];
-  let countriesFound = 0;
-  let locationsFound = 0;
-  let myObject = {};
-  const readline = readLine.createInterface({
-    input: fs.createReadStream(fileName)
-  });
+  importLocations(fileName) {
+    let columnNames = [
+      'change',
+      'country',
+      'location',
+      'name',
+      'nameSansDiacritics',
+      'subdivision',
+      'function',
+      'status',
+      'date',
+      'iata',
+      'coordinates',
+      'remarks'
+    ];
+    let _this = this;
+    let countriesFound = 0;
+    let locationsFound = 0;
+    let myObject = {};
+    const readline = readLine.createInterface({
+      input: fs.createReadStream(fileName)
+    });
 
-  /* Read a line from the input stream/file */
-  readline.on('line', function(line) {
-    let lineRead = [];
-    line = _.replace(line, /"/g, '');
-    lineRead = _.fromPairs(_.zip(columnNames, _.split(line, ',')));
+    /* Read a line from the input stream/file */
+    readline.on('line', function(line) {
+      let lineRead = [];
+      line = _.replace(line, /"/g, '');
+      lineRead = _.fromPairs(_.zip(columnNames, _.split(line, ',')));
 
-    // Check to see if we have found a new country
-    if (lineRead.country != myObject._id) {
-      if (countriesFound) {
-        // Do something with the now complete UNLOCODE country collection
-        eventEmitter.emit('recordFound', JSON.stringify(myObject, null, 3) );
-        //   console.log(JSON.stringify(myObject,null,3));
-        console.log(
-          myObject.country + ' had ' + myObject.locations.length + ' locations'
-        );
-        locationsFound = 0;
+      // Check to see if we have found a new country
+      if (lineRead.country != myObject._id) {
+        if (countriesFound) {
+          // emit the now complete UNLOCODE country collection
+          _this.emit('recordFound', JSON.stringify(myObject, null, 3));
+          locationsFound = 0; //  Reset the location index for the new country
+        }
+
+        myObject._id = lineRead.country;
+        myObject.country = lineRead.name;
+        myObject.locations = _.remove(myObject.locations, true);
+        countriesFound += 1;
+      } else {
+        myObject.locations.push({});
+        myObject.locations[locationsFound].location = lineRead.location;
+        myObject.locations[locationsFound].name = lineRead.name;
+        myObject.locations[locationsFound].details = lineRead;
+        locationsFound += 1;
       }
-
-      myObject._id = lineRead.country;
-      myObject.country = lineRead.name;
-      myObject.locations = _.remove(myObject.locations, true);
-      countriesFound += 1;
-    } else {
-      myObject.locations.push({});
-      myObject.locations[locationsFound].location = lineRead.location;
-      myObject.locations[locationsFound].name = lineRead.name;
-      myObject.locations[locationsFound].details = lineRead;
-      locationsFound += 1;
-    }
-  });
-  readline.on('close', function() {
-    console.log('done!');
-    console.log(countriesFound + ' countries found');
-  });
-  return;
+    });
+    readline.on('close', function() {
+      _this.emit('done', countriesFound + ' countries found');
+    });
+    return;
+  }
 }
-
+module.exports = GetLocations;
