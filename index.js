5347b24c8623b02c9fb93f3e5d63e20f09353d43
Davie Robertson <me@davie.com>
2017-10-02 09:05:00 +0400

    added comments

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ GetLocations.on('recordFound', (data) => {
   /**
  * A complete country and associated locations have been returned in 
  * a JSON string (data). These can now be added to a NONSQL document 
+ * 
+ * your code here could add record to your DB for each country
  */
 
   // console.log(`Received data: "${data}"`);
 });
-
 
 GetLocations.on('done', (message) => {
 /*
